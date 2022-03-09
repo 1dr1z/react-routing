811b00ff026822c2c6be64f21a42d0a5797180ac
Idriz Mehmedovic <idriz@Idrizs-MacBook-Pro.local>
2022-03-09 10:34:40 +0100

    upgrading router to V6

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Products = () => {
+  // const navigate=useNavigate();
+  // navigate('/welcome'); PUSH
+  // navigate('/welcome', {replace:true}); REPLACE
+  // navigate(-1); PREVIOUS PAGE
+  // navigate(1); NEXT PAGE
   return (
     <section>
       <h1>The Products Page</h1>
