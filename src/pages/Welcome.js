a9fc44dfb52c134bb8c30884d8b2b7099befebab
Idriz Mehmedovic <idriz.96@hotmail.com>
2022-03-14 12:46:39 +0100

    enabling link on welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
-      {/* <Link to='new-user'>New User</Link> */}
+      <Link to='new-user'>New User</Link>
       <Outlet />
     </section>
   );
